2527d97b44651790f29f36733b9fe3e258c5d951
FacuTello <facundostello@hotmail.com>
2026-06-18 16:46:36 -0300

    Listado listo

--- dominio/Disco.cs
+++ dominio/Disco.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace dominio
 {
@@ -10,8 +11,13 @@ namespace dominio
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
+        [Display(Name = "Fecha de lanzamiento")]
         public DateTime FechaLanzamiento { get; set; }
+        [Display(Name = "Cantidad de canciones")]
         public int CantidadCanciones { get; set; }
+        [Display(Name = "Foto Tapa")]
         public string UrlTapa { get; set; }
         public Estilo Estilo { get; set; }
         public TipoEdicion TipoEdicion { get; set; }
